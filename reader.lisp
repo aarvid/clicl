c629d4d9644c289f3d5bda51054d1f7ead14e5dc
aarvid <andy.arvid@gmail.com>
2014-11-06 11:12:54 -0300

    added reader.lisp (in development)

---
+++ reader.lisp
@@ -0,0 +1,9 @@
+;;;; reader.lisp - a lisp reader for clicl
+;; Copyright © 2014 Andrew Arvid Peterson <andy.arvid@gmail.com>
+;; see License.txt
+
+(in-package #:clicl)
+
+(defun $read (sandbox &optional input-stream eof-error-p eof-value recursive-p)
+  (declare (ignore sandbox input-stream eof-error-p eof-value recursive-p)))
+
