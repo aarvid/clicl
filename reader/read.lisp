cb04cf856fafb4e52dbf82a5c0d90978c1e3ffc6
aarvid <andy.arvid@gmail.com>
2015-03-06 10:46:34 -0400

    put return-from on reader macro call in read-upcase-downcase-preserve-decimal

--- reader/read.lisp
+++ reader/read.lisp
@@ -864,8 +864,9 @@
 		       ((let ((type (syntax-type table char)))
 			  (or (eq type 'terminating-macro-char)
 			      (eq type 'non-terminating-macro-char)))
-			(funcall (get-macro-character char)
-                                 stream char))
+			(return-from read-upcase-downcase-preserve-decimal
+                          (funcall (get-macro-character char)
+                                  input-stream char)))
 		       (t
 			(error "don't know what this might be"))))
 	       (error "can't get here a"))
