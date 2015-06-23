6e58b1d8f6ecf6573075e1423e6cc9eef7f335ab
aarvid <andy.arvid@gmail.com>
2015-06-23 10:56:12 -0400

    more format strings

--- printer/cformat.lisp
+++ printer/cformat.lisp
@@ -1,6 +1,11 @@
 (in-package :clicl-printer)
 
-
+(defmacro ensure-char (char default)
+  `(if ,char
+       (if (integerp ,char)
+           (code-char ,char)
+           ,char)
+       ,default))
 
 (defun format (dest control-string &rest arguments)
   (let ((return-value nil))
@@ -129,6 +134,109 @@
   (values (nreverse params) index args-used))
 
 
+
+(defun %format-integer (stream int radix atsign-modifier colon-modifier
+                        mincol padchar commachar)
+
+  ;; initialize defaults
+  (unless mincol (setq mincol 0))
+  (setq padchar (ensure-char padchar #\Space))
+  (setq commachar (ensure-char commachar #\,))
+
+  (let ((*print-base* radix)
+        (*print-radix* nil)
+        s
+        (length 0)
+        sign)
+
+    (if (and atsign-modifier (plusp int))
+        (progn (setf sign #\+) (incf length))
+        (if (minusp int)
+            (progn (setf sign #\-) (incf length) (setf int (- int)))))
+
+    (setq s (with-output-to-string (x) (princ int x)))
+    (incf length (length s))
+    (if colon-modifier
+        (incf length (truncate (1- (length s)) 3)))
+    (if (< length mincol)
+        (dotimes (i (- mincol length))
+          (write-char padchar stream)))
+
+    (if sign (write-char sign stream))
+
+    (if colon-modifier
+        (dotimes (i (length s))
+          (write-char (char s i) stream)
+          (let* ((digits-left (- (length s) (1+ i)))
+                 (digit-pos (mod digits-left 3)))
+            (if (and (zerop digit-pos) (plusp digits-left))
+                (write-char commachar stream))))
+        (princ s stream))))
+
+
+  
+
+
+
+(defun %format-cardinal-number (int stream)
+  (if (zerop int) (return-from %format-cardinal-number (princ "zero" stream)))
+  (if (minusp int) 
+      (progn (princ "negative " stream) (setq int (- int))))
+  (cond
+    ((< int 20)
+     (princ (nth int '("zero" "one" "two" "three" "four" "five" 
+                       "six" "seven" "eight" "nine" "ten"
+                       "eleven" "twelve" "thirteen" "fourteen" "fifteen"
+                       "sixteen" "seventeen" "eighteen" "nineteen")) 
+            stream))
+    ((< int 100)
+     (princ (nth (- (truncate int 10) 2) '("twenty" "thirty" "forty"
+                                           "fifty" "sixty" "seventy"
+                                           "eighty" "ninety"))
+            stream)
+     (if (plusp (mod int 10)) 
+         (progn 
+           (write-char #\- stream)
+           (%format-cardinal-number (mod int 10) stream))))
+    ((< int 1000)
+     (%format-cardinal-number (truncate int 100) stream)
+     (princ " hundred" stream)
+     (if (plusp (mod int 100))
+         (progn  
+           (write-char #\Space stream)              
+           (%format-cardinal-number (mod int 100) stream))))
+    ((< int 1000000)
+     (%format-cardinal-number (truncate int 1000) stream)
+     (princ " thousand" stream)
+     (if (plusp (mod int 1000))
+         (progn  
+           (write-char #\Space stream)              
+           (%format-cardinal-number (mod int 1000) stream))))
+    #|
+                        ((< int 1000000000)
+                         (%format-cardinal-number (truncate int 1000000) stream)
+                         (princ " million" stream)
+                         (if (plusp (mod int 1000000))
+                                (progn  
+                                        (write-char #\Space stream)              
+                                        (%format-cardinal-number (mod int 1000000) stream))))
+|#
+                        (t (princ "billions"))))
+
+
+(defun %format-ordinal-number (int stream)
+  (declare (ignore int))
+  (princ "Sorry" stream))
+
+(defun %format-roman-numeral (int stream)
+  (declare (ignore int))
+  (princ "Sorry" stream))
+
+(defun %format-old-roman-numeral (int stream)
+  (declare (ignore int))
+  (princ "Sorry" stream))
+
+
 ;;; Format dispatch functions take a stream, argument list,
 ;;; @-modifier and :-modifier arguments, followed by any passed
 ;;; parameters. Any passed parameters which are nil should be
@@ -162,35 +270,215 @@
      (unless mincol (setq mincol 0))
      (unless colinc (setq colinc 1))
      (unless minpad (setq minpad 0))
-     (setq padchar (if padchar (if (integerp padchar)
-                                   (code-char padchar)
-                                   padchar)
-                       #\Space))
+     (setq padchar (ensure-char padchar #\Space))
 
      (let ((*print-escape* nil)
            (arg (car args)))
        (if (and (null arg) colon-modifier)
            (setq arg "()"))
-       (if atsign-modifier
-           ;; needto output to string to insert padding in front
-           (let ((s (with-output-to-string (x) (princ arg x)))
-                 length)
-             (dotimes (i minpad) (write-char padchar stream))
-             (setq length (length s))
-             (incf length minpad)
-             (do ()
-                 ((>= length mincol))
-               (dotimes (i colinc) (write-char padchar stream))
-               (incf length colinc))
-             (princ s stream))
-           (let (length (start-pos (cl:file-position stream)))
-             (princ arg stream)
-             (setq length (- (cl:file-position stream) start-pos))
-             (if (< length 0) (setq length 0))
-             (dotimes (i minpad) (write-char padchar stream))
-             (incf length minpad)
-             (do ()
-                 ((>= length mincol))
-               (dotimes (i colinc) (write-char padchar stream))
-               (incf length colinc)))))
+       (let* ((s (with-output-to-string (x) (princ arg x)))
+              (length (length s)))
+         (if atsign-modifier
+             ;; needto output to string to insert padding in front
+             (progn
+               (dotimes (i minpad) (write-char padchar stream))
+               (incf length minpad)
+               (do ()
+                   ((>= length mincol))
+                 (dotimes (i colinc) (write-char padchar stream))
+                 (incf length colinc))
+               (princ s stream))
+             (progn
+               (princ s stream)
+               (dotimes (i minpad) (write-char padchar stream))
+               (incf length minpad)
+               (do ()
+                   ((>= length mincol))
+                 (dotimes (i colinc) (write-char padchar stream))
+                 (incf length colinc))))))
      (1+ index)))
+
+
+(%set-format-dispatch-func
+ #\S
+ #'(lambda (stream args index atsign-modifier colon-modifier control
+            &optional mincol colinc
+                      minpad padchar)
+     (declare (ignore control))
+     (setq args (nthcdr index args))
+     (if (null args)
+         (error "Not enough args for ~AS format directive" #\~))
+
+     ;; initialize defaults
+     (unless mincol (setq mincol 0))
+     (unless colinc (setq colinc 1))
+     (unless minpad (setq minpad 0))
+     (setq padchar (ensure-char padchar #\Space))
+
+     (let ((*print-escape* t)
+           (arg (car args)))
+       (if (and (null arg) colon-modifier)
+           (setq arg "()"))
+       (let* ((s (with-output-to-string (x) (prin1 arg x)))
+              (length (length s)))
+         (if atsign-modifier
+            ;; need to output to string to insert padding in front
+            (progn
+              (dotimes (i minpad) (write-char padchar stream))
+              (incf length minpad)
+              (do ()
+                  ((>= length mincol))
+                (dotimes (i colinc) (write-char padchar stream))
+                (incf length colinc))
+              (princ s stream))
+            (progn
+              (princ s stream)
+              (dotimes (i minpad) (write-char padchar stream))
+              (incf length minpad)
+              (do ()
+                  ((>= length mincol))
+                (dotimes (i colinc) (write-char padchar stream))
+                (incf length colinc))))))
+     (1+ index)))
+
+
+(%set-format-dispatch-func
+ #\D
+ #'(lambda (stream args index atsign-modifier colon-modifier control
+            &optional mincol padchar commachar)
+     (block nil
+       (let ((save-args args))
+         (setq args (nthcdr index args))
+         (if (null args)
+             (error "Not enough args for ~~D format directive"))
+
+         ;; if not an integer use ~A output
+         (if (not (integerp (car args)))
+             (let ((*print-base* 10))
+               (return (funcall (%get-format-dispatch-func #\A)
+                                stream save-args index atsign-modifier
+                                colon-modifier control mincol 1 padchar
+                                commachar))))
+
+         (%format-integer stream (car args) 10 atsign-modifier colon-modifier
+                          mincol padchar commachar)
+         (1+ index)))))
+
+
+(%set-format-dispatch-func
+ #\B
+ #'(lambda (stream args index atsign-modifier colon-modifier control
+            &optional mincol padchar commachar)
+     (declare (ignore control))
+     (block nil
+       (setq args (nthcdr index args))
+       (if (null args)
+           (error "Not enough args for ~AB format directive" #\~))
+
+       ;; if not an integer use ~A output
+       (if (not (integerp (car args)))
+           (let ((*print-base* 2))
+             (return (apply (%get-format-dispatch-func #\A)
+                            stream args atsign-modifier
+                            colon-modifier mincol nil nil padchar))))
+
+       (%format-integer stream (car args) 2 atsign-modifier colon-modifier
+                        mincol padchar commachar)
+       (1+ index))))
+
+(%set-format-dispatch-func
+ #\O
+ #'(lambda (stream args index atsign-modifier colon-modifier control
+            &optional mincol padchar commachar)
+     (declare (ignore control))
+     (block nil
+       (setq args (nthcdr index args))
+       (if (null args)
+           (error "Not enough args for ~AO format directive" #\~))
+
+       ;; if not an integer use ~A output
+       (if (not (integerp (car args)))
+           (let ((*print-base* 8))
+             (return (apply (%get-format-dispatch-func #\A)
+                            stream args atsign-modifier
+                            colon-modifier mincol nil nil padchar))))
+
+       (%format-integer stream (car args) 8 atsign-modifier colon-modifier
+                        mincol padchar commachar)
+       (1+ index))))
+
+(%set-format-dispatch-func
+ #\X
+ #'(lambda (stream args index atsign-modifier colon-modifier control
+            &optional mincol padchar commachar)
+     (declare (ignore control))
+     (block nil
+       (setq args (nthcdr index args))
+       (if (null args)
+           (error "Not enough args for ~AX format directive" #\~))
+
+       ;; if not an integer use ~A output
+       (if (not (integerp (car args)))
+           (let ((*print-base* 16))
+             (return (apply (%get-format-dispatch-func #\A)
+                            stream args atsign-modifier
+                            colon-modifier mincol nil nil padchar))))
+
+       (%format-integer stream (car args) 16 atsign-modifier colon-modifier
+                        mincol padchar commachar)
+       (1+ index))))
+
+(%set-format-dispatch-func
+ #\R
+ #'(lambda (stream args index atsign-modifier colon-modifier control
+            &optional radix mincol padchar commachar)
+     (declare (ignore control))
+     (block nil
+       (setq args (nthcdr index args))
+       (if (null args)
+           (error "Not enough args for ~AR format directive" #\~))
+
+       (if radix
+           ;; if not an integer use ~A output
+           (progn
+             (if (not (integerp (car args)))
+                 (let ((*print-base* radix))
+                   (return (apply (%get-format-dispatch-func #\A)
+                                  args atsign-modifier
+                                  colon-modifier mincol nil nil padchar))))
+             (unless (and (plusp radix) (<= radix 36))
+               (error "Invalid radix specified: ~A" radix))
+             (%format-integer stream (car args) radix atsign-modifier colon-modifier
+                              mincol padchar commachar))
+           (progn
+             (if (not (integerp (car args)))
+                 (return (apply (%get-format-dispatch-func #\A)
+                                args atsign-modifier
+                                colon-modifier mincol nil nil padchar)))
+             (cond
+               ((and atsign-modifier colon-modifier)
+                (%format-old-roman-numeral (car args) stream))
+               (atsign-modifier (%format-roman-numeral (car args) stream))
+               (colon-modifier (%format-ordinal-number (car args) stream))
+               (t (%format-cardinal-number (car args) stream)))))
+       (1+ index))))
+
+(%set-format-dispatch-func
+ #\~
+ #'(lambda (stream args index atsign-modifier colon-modifier control
+            &optional num)
+     (declare (ignore args atsign-modifier colon-modifier control))
+     (unless num (setq num 1))
+     (dotimes (i num)
+       (write-char #\~ stream))
+     index))
+
+(%set-format-dispatch-func
+ #\%
+ #'(lambda (stream args index atsign-modifier colon-modifier control
+            &optional num)
+     (declare (ignore args atsign-modifier colon-modifier control))
+     (unless num (setq num 1))
+     (dotimes (i num)
+       (write-char #\Newline stream))
+     index))
